5f2c35d3047edb3746ad87242b7352fd43c8e584
wdhanhuan <wdhanhuan@users.github.com>
2018-06-06 14:40:39 +0800

    java

---
+++ src/org/hans/java/Java.java
@@ -0,0 +1,10 @@
+package org.hans.java;
+
+public class Java {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("hahah");
+	}
+
+}
---
+++ src/org/hans/java/JavaTest.java
@@ -0,0 +1,10 @@
+package org.hans.java;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		System.out.println("dadada");
+
+	}
+
+}
