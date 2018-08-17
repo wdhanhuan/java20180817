03fac5e097c1f287f314c6418d34fb14b48ba91a
wdhanhuan <wdhanhuan@users.github.com>
2018-08-17 15:37:57 +0800

    changeOne

--- src/org/han/applet/CheckDemo.java
+++ src/org/han/applet/CheckDemo.java
@@ -2,21 +2,6 @@ package org.han.applet;
 
 public class CheckDemo {
 	public static void main(String[] args) {
-		int num20177=4603;
-		int num20178=15663;
-		int num20179=7130;
-		int num201710=7310;
-		int num201711=12692;
-		int num201712=7130;
-		int num20181=7130;
-		int num20182=18610;
-		int num20183=8308;
-		int num20184=7130;
-		int num20185=7580;
-		int num20186=7580;
-		int num20187=7828;
-		int num20188=6999;
-		int sum=num20177+num20178+num20179+num201710+num201711+num201712+num20181+num20182+num20183+num20184+num20185+num20186+num20187+num20188;
-		System.out.println("getAll:\t"+sum);
+		System.out.println("a");
 	}
 }
